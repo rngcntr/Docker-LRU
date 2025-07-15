820364d322953f9e1242695747632343252ef0f7
Florian Grieskamp <florian.grieskamp@gdata.de>
2025-07-15 13:24:12 +0200

    Include full image name in output of list-recent-images.sh

--- scripts/list-recent-images.sh
+++ scripts/list-recent-images.sh
@@ -6,6 +6,6 @@ mkdir -p "/root/.docker-lru/images"
 find "/root/.docker-lru/images/" -maxdepth 1 -type f | while read -r hash; do
     timestamp=$(cat "$hash" | cut -d ' ' -f 1)
     image=$(cat "$hash" | cut -d ' ' -f 2)
-    echo "$timestamp $(basename "$image")"
+    echo "$timestamp $image"
 # Sort by most recent usage, beginning with the oldest image
 done | sort -nk 1 | awk '{print $2}'
